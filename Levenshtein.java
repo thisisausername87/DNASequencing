bc2a16bdc14d55de2da601353b557631d14927a1
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2026-03-17 03:11:42 -0600

    Create Levenshtein.java

---
+++ Levenshtein.java
@@ -0,0 +1,43 @@
+public class Levenshtein implements SequenceScoringAlgorithm {
+
+    @Override
+    public String getName() {
+        return "Levenshtein Distance";
+    }
+
+    @Override
+    public double score(String databaseSequence, String querySequence) {
+        int m = databaseSequence.length();
+        int n = querySequence.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cost = (databaseSequence.charAt(i - 1) == querySequence.charAt(j - 1)) ? 0 : 1;
+
+                dp[i][j] = Math.min(
+                    Math.min(
+                        dp[i - 1][j] + 1,
+                        dp[i][j - 1] + 1
+                    ),
+                    dp[i - 1][j - 1] + cost
+                );
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    @Override
+    public boolean higherScoreIsBetter() {
+        return false;
+    }
+}
